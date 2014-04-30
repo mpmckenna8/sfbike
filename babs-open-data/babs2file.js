efb729b6cb2e4c6afbe918b2ff31804dcda90237
Matthew McKenna <mpmckenna8@me.com>
2014-04-30 08:19:37 -0700

    oops adding all of d3 and some other stuff...

--- babs-open-data/babs2file.js
+++ babs-open-data/babs2file.js
@@ -5,8 +5,8 @@ var request = require('request');
 var babs =   request('http://bayareabikeshare.com/stations/json', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var obj = JSON.parse(body);
-
-            var stories = obj.stationBeanList.map(function (story) { return story.city; });
+var stats = [];
+            var stories = obj.stationBeanList.map(function (story) { if(story.stationName){return story; }});
             var when = obj.executionTime;
             console.log(when);
             console.log(stories)
